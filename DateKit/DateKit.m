be542765eed408936004bbfbd774ba826f83dbb5
伊藤大司 <dj110@itoudaishis-Mac-mini.local>
2014-03-01 08:55:16 +0800

    Initial Commit

---
+++ DateKit/DateKit.m
@@ -0,0 +1,13 @@
+//
+//  DateKit.m
+//  DateKit
+//
+//  Created by 伊藤大司 on 3/1/14.
+//  Copyright (c) 2014 DJ110. All rights reserved.
+//
+
+#import "DateKit.h"
+
+@implementation DateKit
+
+@end
---
+++ DateKitTests/DateKitTests.m
@@ -0,0 +1,34 @@
+//
+//  DateKitTests.m
+//  DateKitTests
+//
+//  Created by 伊藤大司 on 3/1/14.
+//  Copyright (c) 2014 DJ110. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DateKitTests : XCTestCase
+
+@end
+
+@implementation DateKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
